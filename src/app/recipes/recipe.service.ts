4b9217ddaad883408b6aceb3f1f4bc042a5e978c
Marko Kozjak <kozypato@gmail.com>
2017-12-27 16:33:35 +0100

    Upgrading to using services across the app and sharing data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,16 +1,29 @@
 import {Recipe} from './recipe.model';
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('A test Revipe','This is simply a test','http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
-        new Recipe('A test Revipe','Another test recipe','http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg')
+        new Recipe('Schnitzel',
+            'This is simply a test',
+            'http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg', [
+                new Ingredient('Meat', 1), new Ingredient('French fries', 20)
+            ]),
+        new Recipe('Spheggeti','Another test recipe','http://maxpixel.freegreatpicture.com/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg', [])
     ];
+
+    constructor(private slService: ShoppingListService){}
 
     getRecipes() {
         // slice so we get a copy and not a direct instance of object / aray
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]){
+        this.slService.addIngredients(ingredients);
     }
 
 }
