66b0377bee7be4b8244e0f3067150db68e12a0ed
Marko Kozjak <kozypato@gmail.com>
2017-12-29 11:35:11 +0100

    Small upgrades (unsubscribe, remove ingredients...)

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -48,9 +48,14 @@ export class RecipeEditComponent implements OnInit {
         );
     }
 
+    onDeleteIngredient(index: number){
+        (<FormArray>this.recipeForm.get('ingredients')).removeAt(index);
+    }
+
     onCancel() {
       this.router.navigate(['../'], {relativeTo: this.route});
     }
+
 
   private initForm() {
 
